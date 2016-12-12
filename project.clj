53b44f0994f8f5349e2b0f27517fdf3c2d936262
Mohammad El-Abid <mohammad.elabid@gmail.com>
2016-12-11 22:17:45 -0800

    Move over to MIT license.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject emptys-otomatik-plugins "0.2.0-SNAPSHOT" ; version should match irc-bot's version
     :url "https://github.com/TheEmpty/EmptysOtomatikPlugins"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :license {:name "MIT License"
+              :url "http://www.opensource.org/licenses/mit-license.php"}
     :dependencies [
                    [org.clojure/clojure "1.8.0"]
                    [otomatik "0.2.0-SNAPSHOT"]
